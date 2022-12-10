e9574a7d7d7892969238ac53dd5cee2190783827
DanielJ2006 <daniel_1_al@hotmail.com>
2022-12-10 18:01:01 -0500

    itementregas

--- app/src/main/java/idat/dami/prueba/retrofit/AzCourierServicio.kt
+++ app/src/main/java/idat/dami/prueba/retrofit/AzCourierServicio.kt
@@ -1,13 +1,16 @@
 package idat.dami.prueba.retrofit
 
 import idat.dami.prueba.retrofit.response.ResponseConsignado
+import idat.dami.prueba.retrofit.response.ResponseEntrega
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface AzCourierServicio {
 
-    @GET("listar")
+    @GET("consignado/listar")
     fun lsitarConsignados(): Call<List<ResponseConsignado>>
 
+    @GET("entrega/listar")
+    fun listarEntregas(): Call<List<ResponseEntrega>>
 
 }
